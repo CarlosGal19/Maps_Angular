b8f942608b867eda6f08cdf3b21adbc0fbdbe120
Carlos Galindo <c.galindo.islas@gmail.com>
2026-04-12 19:28:02 -0600

    feat: create main pages

---
+++ src/app/pages/full-screen-map-page/full-screen-map-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'full-screen-map-page',
+  imports: [],
+  templateUrl: './full-screen-map-page.html',
+})
+export class FullScreenMapPage {
+
+}
