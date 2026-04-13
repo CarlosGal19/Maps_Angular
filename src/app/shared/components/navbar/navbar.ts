330dc44dd91bec71ff5ad13345fd9eae74400933
Carlos Galindo <c.galindo.islas@gmail.com>
2026-04-12 20:12:45 -0600

    feat: show title from route in navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './navbar.html',
 })
 export class Navbar {
+
+  router = inject(Router);
+
+  routes = routes
+    .map(r => ({
+      path: r.path,
+      title: `${r.title ?? 'Angular maps'}`
+    }))
+    .filter(r => r.path !== '**')
+
+  pageTitle = this.router.events.pipe(
+    filter(data => data instanceof NavigationEnd),
+    // tap((data) => console.log(data)),
+    map(data => data.url),
+    map(data => {
+      return this.routes.find(r => `/${r.path}` == data)!.title
+    })
+  )
 
 }
