5f7528c133ec692957a7afcf267df1d3f2aee1c6
Carlos Galindo <c.galindo.islas@gmail.com>
2026-04-12 19:29:11 -0600

    feat: set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullScreenMapPage } from './pages/full-screen-map-page/full-screen-map-page';
+import { HousesPage } from './pages/houses-page/houses-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullScreenMapPage,
+    title: 'Full Screen Map'
+  },
+  {
+    path: 'houses',
+    component: HousesPage,
+    title: 'Houses'
+  },
+  {
+    path: 'markers',
+    component: MarkersPage,
+    title: 'Markers'
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  }
+];
